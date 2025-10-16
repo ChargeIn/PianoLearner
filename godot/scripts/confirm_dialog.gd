1d820145ddeeea58737c1f5c2aae625e06d59e67
Florian Plesker <florian.plesker@web.de>
2025-10-16 22:43:23 +0200

    Further progress

--- godot/scripts/confirm_dialog.gd
+++ godot/scripts/confirm_dialog.gd
@@ -12,4 +12,22 @@ var is_open: bool = false
 
 func _ready() -> void:
 	set_process_unhandled_key_input(false)
-	action_button.pressed.connect() 
+	action_button.pressed.connect(_on_action_pressed)
+	cacnel_button.pressed.connect(_on_cancel_pressed)
+	hide()
+	
+func _unhandled_key_input(event: InputEvent) -> void:
+	if event.is_action_pressed("ui_cancel"):
+		cancel()
+		
+func _close_modal(is_confirmed: bool) -> void:
+	set_process_unhandled_input(false)
+	confirmed.emit(is_confirmed);
+	set_deferred('is_open', false)
+			
+		
+func _on_action_pressed() -> void:
+	pass
+	
+func _on_cancel_pressed() -> void:
+	pass
