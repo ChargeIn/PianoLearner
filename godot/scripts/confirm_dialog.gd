33bb317721ae5c72016589f07ff44c931aa73256
Florian Plesker <florian.plesker@web.de>
2025-10-19 22:34:52 +0200

    Added dialog customization

--- godot/scripts/confirm_dialog.gd
+++ godot/scripts/confirm_dialog.gd
@@ -5,7 +5,7 @@ signal confirmed(is_confirmed: bool)
 @onready var header_label: Label = %HeaderLabel
 @onready var message_label: Label = %MessageLabel
 
-@onready var cacnel_button: Button = %CancelButton
+@onready var cancel_button: Button = %CancelButton
 @onready var action_button: Button = %ActionButton
 
 var is_open: bool = false
@@ -13,21 +13,35 @@ var is_open: bool = false
 func _ready() -> void:
 	set_process_unhandled_key_input(false)
 	action_button.pressed.connect(_on_action_pressed)
-	cacnel_button.pressed.connect(_on_cancel_pressed)
+	cancel_button.pressed.connect(_on_cancel_pressed)
 	hide()
 	
 func _unhandled_key_input(event: InputEvent) -> void:
 	if event.is_action_pressed("ui_cancel"):
 		cancel()
 		
+func customize(header: String, message: String, actionText: String, cancelText: String) -> ConfirmDialog:
+	header_label.text = header
+	message_label.text = message
+	action_button.text = actionText
+	cancel_button.text = cancelText
+	return self
+		
+func confirm() -> void:
+	_close_modal(true)
+	
+func cancel() -> void:
+	_close_modal(false)
+		
 func _close_modal(is_confirmed: bool) -> void:
 	set_process_unhandled_input(false)
 	confirmed.emit(is_confirmed);
 	set_deferred('is_open', false)
+	hide()
 			
 		
 func _on_action_pressed() -> void:
-	pass
+	confirm()
 	
 func _on_cancel_pressed() -> void:
-	pass
+	cancel()
